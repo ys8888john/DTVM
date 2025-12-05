a1d0d5a9e69e0ae58bcd1e691815c99e97574706
ChenLi <cl507523@antgroup.com>
2025-12-05 21:45:33 +0800

    feat: add statistic for EVM execution (#206)

--- src/runtime/runtime.cpp
+++ src/runtime/runtime.cpp
@@ -672,6 +672,9 @@ void Runtime::callEVMInInterpMode(EVMInstance &Inst, evmc_message &Msg,
 
 void Runtime::callEVMMain(EVMInstance &Inst, evmc_message &Msg,
                           evmc::Result &Result) {
+#ifdef ZEN_ENABLE_LINUX_PERF
+  auto Timer = Stats.startRecord(utils::StatisticPhase::Execution);
+#endif
   evmc_message MsgWithCode = Msg;
   MsgWithCode.code = reinterpret_cast<uint8_t *>(Inst.getModule()->Code);
   MsgWithCode.code_size = Inst.getModule()->CodeSize;
@@ -692,6 +695,9 @@ void Runtime::callEVMMain(EVMInstance &Inst, evmc_message &Msg,
         zen::utils::toHex(Result.output_data, Result.output_size);
     ZEN_LOG_INFO("output: 0x%s", output.c_str());
   }
+#ifdef ZEN_ENABLE_LINUX_PERF
+  Stats.stopRecord(Timer);
+#endif
 }
 #endif // ZEN_ENABLE_EVM
 
